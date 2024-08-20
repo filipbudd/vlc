00c9ac0fa6b4dda0d1196106a973fa62017c74eb
Filip Budd <57685558+filipbudd@users.noreply.github.com>
2024-08-19 21:19:45 -0700

    Added the module information
    Added the module information and deleted int main() because I don't think it will be needed.

--- modules/audio_selector/audio_selector.c
+++ modules/audio_selector/audio_selector.c
@@ -6,7 +6,10 @@
 #include <vlc_plugin.h>
 #include <vlc_interface.h>
 
-int main() {
-    printf("Hello World!\n");
-	return 0;
-}
+vlc_module_begin()
+    set_shortname(N_("Audio Track Assigner"))
+    set_description(N_("Allows the user to assign different audio tracks to different output devices."))
+    set_capability("audio output", 0) //other potential options may be "audio filter" or "audio mixer" https://wiki.videolan.org/Documentation:VLC_Modules_Loading/#How
+    set_callbacks(Open, Close)
+    set_category(CAT_AUDIO)
+vlc_module_end()
