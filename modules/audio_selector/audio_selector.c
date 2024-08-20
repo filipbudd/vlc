f5785708434cde0eef6539240742f209007f8734
Filip Budd <57685558+filipbudd@users.noreply.github.com>
2024-08-19 20:56:53 -0700

    Added VLC API Headers

--- modules/audio_selector/audio_selector.c
+++ modules/audio_selector/audio_selector.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/*VLC Core API Headers*/
+#include <vlc_common.h>
+#include <vlc_plugin.h>
+#include <vlc_interface.h>
 
 int main() {
     printf("Hello World!\n");
